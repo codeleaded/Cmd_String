9e4d5ec09dd36aedc444a4db29afc8e24cd6f09e
codeleaded <codeleaded@gmail.com>
2025-11-06 02:05:39 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,14 @@
-#include "C:/Wichtig/System/Static/Library/String.h"
+#if defined(__linux__)
+    #include "/home/codeleaded/System/Static/Library/String.h"
+#elif defined(_WINE)
+    #include "/home/codeleaded/System/Static/Library/String.h"
+#elif defined(_WIN32)
+    #include "F:/home/codeleaded/System/Static/Library/String.h"
+#elif defined(__APPLE__)
+    #error "Apple not supported!"
+#else
+    #error "Platform not supported!"
+#endif
 
 int main(){
     
